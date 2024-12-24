59d8fe0824589c35cc4fb678344d417d04a4d11e
Grigary1 <grigaryvarkey16@gmail.com>
2024-12-24 18:42:03 +0530

    updates

--- app/(defaultPage)/index.tsx
+++ app/(defaultPage)/index.tsx
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Category from './../(defaultPage)/(HomeScreen)/CategoryList.jsx'
 import Header from './../(defaultPage)/(HomeScreen)/Header'
 
 const index = () => {
   return (
     <View className='p-8'>
       <Header/>
+      <Category/>
     </View>
   )
 }
