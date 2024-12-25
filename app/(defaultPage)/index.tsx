8e7fc64ab03fa7ab956e1248db57edfce2a79055
Grigary1 <grigaryvarkey16@gmail.com>
2024-12-25 22:39:28 +0530

    category updated

--- app/(defaultPage)/index.tsx
+++ app/(defaultPage)/index.tsx
@@ -1,17 +1,34 @@
 import { View } from 'react-native';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import getCategory from './../../constants/GlobalApi';
 import Category from './../(defaultPage)/(HomeScreen)/CategoryList.jsx';
 import Header from './../(defaultPage)/(HomeScreen)/Header';
 
 const Index = () => {
-  const [categories, setCategories] = useState([]); // State to store categories
+  const [categories,setCategories]=useState();
+  
+      useEffect(()=>{
+          getCategoryData();
+      },[])
+  
+      const getCategoryData = () => {
+          getCategory().then(response => {
+                  console.log(response); // Log the response
+                  setCategories(response);
+              })
+              .catch(error => {
+                  console.error("Error fetching category:", error); // Handle the error
+              });
+      }; // State to store categories
 
+      console.log("index cate",categories);
   return (
     <View className="p-8">
       {/* Pass the setCategories function to Header */}
-      <Header setCategories={setCategories} />
+      <Header/>
       
       {/* Pass the categories to Category component */}
+
       <Category categories={categories} />
     </View>
   );
